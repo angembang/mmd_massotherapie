4757d85dfc59809efedb0b00b1d6eeb0ef71bb99
Ange <angembang@gmail.com>
2026-07-14 00:34:34 +0200

    chore(seo): add robots.txt and sitemap.xml configuration

--- frontend/src/app/constants/business.constants.ts
+++ frontend/src/app/constants/business.constants.ts
@@ -2,7 +2,7 @@ export const BUSINESS = {
   phoneDisplay: '06 03 66 76 19',
   phoneLink: 'tel:+33603667619',
   instagramHandle: 'mmd.massotherapeute',
-  instagramLink: 'https://instagram.com/mmd.massotherapeute',
+  instagramLink: 'https://www.instagram.com/mmd.massotherapeute',
   email: 'mmd.massotherapie@gmail.com',
   address: {
     street: '5 rue du Bosphore',
